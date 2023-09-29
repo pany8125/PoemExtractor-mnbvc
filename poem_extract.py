e4d4cfbb727e57b4565db93fdd8fdd8602c24b55
Scott <pany-8125@163.com>
2023-09-29 08:10:49 +0800

    诗歌标准化完成

--- poem_extract.py
+++ poem_extract.py
@@ -3,146 +3,59 @@
 
 from datetime import datetime
 import json
-from enum import Enum
+import os
+import logging
+import hashlib
 import schema
-import re
 
+# 配置日志记录
+logging.basicConfig(
+    filename='poem_log_file.log',  # 指定日志文件的名称
+    level=logging.WARNING,  # 指定日志级别（INFO、WARNING、ERROR、CRITICAL等）
+    format='%(asctime)s [%(levelname)s]: %(message)s',  # 日志格式
+    datefmt='%Y-%m-%d %H:%M:%S'  # 日期和时间格式
+)    
 
-
-# 读取文件
-# file_path_all = 'final_data_sample_230706test.json'  # 替换为实际文件路径
-
-# 获取每一段对话，输入到json中处理
-def process_json_file(file_path, write_file, start_line=1):
+def process_poem_file(file_path, output_file, start_line):
+    file_name = os.path.basename(file_path)
+    file_size = os.path.getsize(file_path)
     with open(file_path, 'r', encoding='utf-8') as f:
-        # 定位到指定行数
-        for _ in range(start_line - 1):
-            f.readline()
-        buffer = ""
-        json_len = 0
-        json_str_flag = Json_str.NONE.value # 检测到json串时修改为对应状态，检测完成修改回NONE
         for line_number, line in enumerate(f, start=start_line):
-            # 打印迭代信息
-            # print(f"Line {line_number}: {line}")
-            # 如果json_len大于10，就退出
-            if json_len > 10:
-                break
             this_line = line.strip()
-            if json_str_flag == Json_str.NONE.value:
-                if this_line == "[":
-                    print(f"Line {line_number}, start of text!")  # json检测文件开始
-                    continue
-                elif this_line == "]":
-                    print(f"Line {line_number}, end of text!")  # json检测还没开始就到末尾了
-                    break 
-                elif this_line != "{":
-                    print(f"Line {line_number}, error!")  # json检测失败
-                    break
-                elif this_line == Json_str.JSON_START.value:
-                    print(f"Line {line_number}, start parsing json!")  # json解析开始
-                    json_str_flag = Json_str.JSON_START.value
-                    buffer += this_line
-                    continue
-                else:
-                    print(f"Line {line_number}, error!")  # json检测失败
-                    print(f"parse stage: {json_str_flag}, json str: {buffer}")
-                    break
-            if json_str_flag == Json_str.JSON_START.value:
-                if this_line.startswith(Json_str.ID.value):
-                    print(f"Line {line_number}, id detected!")  # json解析开始
-                    json_str_flag = Json_str.ID.value
-                    buffer += this_line
-                    continue
-                else:
-                    print(f"Line {line_number}, error!")  # json检测失败
-                    print(f"parse stage: {json_str_flag}, json str: {buffer}")
-                    break
-            if json_str_flag == Json_str.ID.value:
-                if this_line.startswith(Json_str.CONVERSATION_START.value):
-                    print(f"Line {line_number}, conversations detected!")  # json解析开始
-                    json_str_flag = Json_str.CONVERSATION_START.value
-                    buffer += this_line
-                    continue
-                else:
-                    print(f"Line {line_number}, error!")  # json检测失败
-                    print(f"parse stage: {json_str_flag}, json str: {buffer}")
-                    break
-            if json_str_flag == Json_str.CONVERSATION_START.value:
-                if this_line == Json_str.CONVERSATION_END.value:
-                    print(f"Line {line_number}, conversations end!")  
-                    json_str_flag = Json_str.CONVERSATION_END.value
-                    buffer += this_line
-                    continue
-                else:
-                    print(f"Line {line_number}, conversations parsing!")
-                    # TODO：检测下buffer长度，以免数据异常
-                    buffer += this_line
-                    continue
-            if json_str_flag == Json_str.CONVERSATION_END.value:
-                if this_line == Json_str.JSON_END.value or this_line == Json_str.JSON_END_END.value:
-                    print(f"Line {line_number}, json end!")  # json解析开始
-                    json_str_flag = Json_str.NONE.value
-                    buffer += '}'
-                    if process_json(buffer, write_file):
-                        buffer = ''
-                        json_len += 1
-                        continue
-                    else:
-                        print(f"Line {line_number}, error!")  # json检测失败
-                        print(f"parse stage: {json_str_flag}, json str: {buffer}")
-                        break
-                else:
-                    print(f"Line {line_number}, error!")  # json检测失败
-                    print(f"parse stage: {json_str_flag}, json str: {buffer}")
-                    break
-            # 撒分支都没走进去
-            print(f"Line {line_number}, error!")  # json检测失败
-            print(f"parse stage: {json_str_flag}, json str: {buffer}")
-
-def process_json(json_str, write_file=None):
-    # Check if str is a valid JSON
-    try:
-        json_data = json.loads(json_str)
-        id = json_data['id']
-        conversation = json_data['conversations']
-        # 打印conversation的长度，并加上说明
-        print(f"conversation length: {len(conversation)}")
-        # 判断conversation的长度是否为偶数，如果是奇数，就退出
-        if(len(conversation) % 2 != 0):
-            return False
-        for i in range(int(len(conversation)/2)):
-            # 如果是第一个元素，就是问题
-            if(conversation[i*2]['from'] != 'human'):
-                return False
-            # 如果是第二个元素，就是答案
-            if(conversation[i*2+1]['from'] != 'gpt'):
-                return False
-            # 如果是第一个元素，就是问题
-            question = conversation[i*2]['value']
-            # 如果是第二个元素，就是答案
-            answer = conversation[i*2+1]['value']
-            lang = 'not_en_or_zh'
-            # 确认是否英文或中文，可能带有标点符号
-            if re.match(r'^[a-zA-Z0-9\.\,\?\!\s]+$', question) is not None:
-                lang = 'en'
-            elif re.match(r'^[\u4e00-\u9fa5\.\,\?\!\s]+$', question) is not None:
-                lang = 'zh'
-            # 生成json
-            json_str = schema.ShareGPTQASchema(id, question, answer, id, i+1, lang).to_json()
-            write_file.write(json_str)
-            write_file.write('\n')
-        return True
-    except json.JSONDecodeError:
-        print("JSONDecodeError")
-        return False
+            # 跳过空行
+            if len(this_line) == 0:
+                continue
+            # 对当前行转换为json，如果报错则跳过
+            try:
+                json_data = json.loads(this_line)
+                #用json_data的md5值作为id
+                md5 = hashlib.md5(this_line.encode('utf-8')).hexdigest()
+                json_str = schema.POEMSchema(file_name, file_size, line_number, md5, json_data['title'], json_data['author'], json_data['content']).to_json()
+                output_file.write(json_str + '\n')
+            except json.decoder.JSONDecodeError:
+                logging.error(f"Line {line_number}, json decode error!")  # json解析失败
+                logging.error(f"json str: {this_line}")
+                continue
+            except KeyError:
+                logging.error(f"Line {line_number}, key error!")
+                logging.error(f"json str: {this_line}")
+                continue
+            except Exception as e:
+                logging.error(f"Line {line_number}, unknown error!")
+                logging.error(f"json str: {this_line}")
+                logging.error(e)
+                continue
+            # 打印迭代信息
+            logging.info(f"Line {line_number} processed successfully!")
+            logging.info(f"json str: {this_line}")
 
 if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser(description="Parse shareGPT into QA data.")
+    parser = argparse.ArgumentParser(description="Parse poem text to MNBVC text format.")
     parser.add_argument("source_files",type=str, default="final_data_sample_230706test.json", help="文件名")
-    parser.add_argument("-o","--output",type=str, default="shareGPT",help="output file name (without extension)")
+    parser.add_argument("-o","--output",type=str, default="POEM",help="output file name (without extension)")
     parser.add_argument("-l","--start_line",type=int,default=1,help="read start line")
     args = parser.parse_args()
     with open(f'{args.output}_{datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.jsonl', 'w', encoding='utf-8') as of:
         # 调用函数来处理 JSON 文件，默认从第1行开始读取
-        process_json_file(args.source_files, of, args.start_line)
+        process_poem_file(args.source_files, of, args.start_line)
--- schema.py
+++ schema.py
@@ -1,38 +1,40 @@
 import json
 from datetime import datetime
 
-class ShareGPTQASchema:
-    def __init__(self, id, question, answer, session, round_number, lang):
-        self.id = id
-        self.question = question
-        self.answer = answer
-        self.source = "ShareGPT"
-        self.create_time = datetime.now().strftime("%Y%m%d %H:%M:%S")
-        self.question_detail = "\"from\": \"human\""
-        self.answer_detail = "\"from\": \"gpt\""
-        self.session = session
-        self.round_number = round_number
-        self.lang = lang
+class POEMSchema:
+    def __init__(self, file_name, file_size, line_number, md5, title, author, content):
+        self.file_name = file_name
+        self.file_size = file_size
+        self.line_number = line_number
+        self.md5 = md5
+        self.title = title
+        self.author = author
+        self.content = content
 
     def to_json(self):
         data = {
-            "id": self.id,
-            "问": self.question,
-            "答": self.answer,
-            "来源": self.source,
-            "元数据": {
-                "create_time": self.create_time,
-                "问题明细": self.question_detail,
-                "回答明细": self.answer_detail,
-                "扩展字段": {
-                    "会话": self.session,
-                    "多轮序号": self.round_number,
-                    "语言": self.lang
-                }
+                "文件名": self.file_name,
+                "是否待查文件": False,
+                "是否重复文件": False,
+                "文件大小": self.file_size,
+                "simhash": 0,
+                "最长段落长度": 0,
+                "段落数": 0,
+                "去重段落数": 0,
+                "低质量段落数": 0,
+                "段落": [
+                    {
+                    '行号': self.line_number,
+                    '是否重复': False,
+                    '是否跨文件重复': False,
+                    'md5': self.md5, #整行json的md5
+                    '标题': self.title, # 对应title
+                    '作者': self.author, # 对应author
+                    '内容': self.content # 对应content
+                    }
+                ]
             }
-        }
         return json.dumps(data, separators=(",", ":"), ensure_ascii=False)
 
-json_str = ShareGPTQASchema(0, "Can you make me a Shakespearean script about a girl who has tummy troubles and can't fart not matter how hard she tries- so they think she is a witch",
-                "Sure, here's a Shakespearean script about a girl who c...", "ShareGPT", 1, 'en').to_json()
-print(json_str)
+json_str = POEMSchema("chinese_poems.jsonl",123,2,"afgdsfds","吃方便面想的一些问题222","22200913","\n222方便面快速解决饥饿\n饥饿可以慢慢地减退\n方便面。。。").to_json()
+# print(json_str)
